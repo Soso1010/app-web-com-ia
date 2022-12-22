1a9e211b7c3ef035f3d948409ef3bb7bb2d30cf8
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2022-12-22 17:59:24 -0300

    Update maIn.js

--- maIn.js
+++ maIn.js
@@ -31,42 +31,26 @@ function draw(){
     if(scoreLeftWrist > 0.2)
     {
    circle(leftWristX, leftWristY,20);
-    InNumberleftWristY = Number(leftWristY);
-    remove_decimals = floor(InNumberleftWristY);
-    volume = remove_decimals/500;
-    document.getElementById("volume").innerHTML = "Volume = " + volume;
-    song.setVolume(volume);
+    song1.stop();
+        if(song2_status == false)
+        {
+song2.play();
+            document.getElementById("song").innerHTML = "Tocando: Música de Peter Pan";
     }
 
     if(scoreRightWrist > 0.2)
     {
    circle(rightWristX, rightWristY,20);
+        song2.stop();
+        if(song1_status == false)
+        {
+song1.play();
+            document.getElementById("song").innerHTML = "Tocando: Música de Harry Potter";
+    }
    
-   if(rightWristY >0 && rightWristY <= 100)
-   document.getElementById("speed").innerHTML = "Velocidade = 0.5x";
-   song.rate(0.5);
+   
     }
-    else if(rightWristY >100 && rightWristY <= 200)
-    {
-        document.getElementById("speed").innerHTML = "Velocidade = 1x";
-        song.rate(1);
-    }
-    else if(rightWristY >200 && rightWristY <= 300)
-    {
-        document.getElementById("speed").innerHTML = "Velocidade = 1.5x";
-        song.rate(1.5);
-    }
-    else if(rightWristY >300 && rightWristY <= 400)
-    {
-        document.getElementById("speed").innerHTML = "Velocidade = 2x";
-        song.rate(2);
-    }
-    else if(rightWristY >400)
-    {
-        document.getElementById("speed").innerHTML = "Velocidade = 2.5x";
-        song.rate(2.5);
-    }
-}
+    
 
 function play()
 {
